fff1b015caa4089b5a0995c1fda01bcb56bf9564
Pablo <pablo.trescoli@yahoo.es>
2020-11-16 12:08:12 +0100

    - zoom reset when landscape fullscreen bug fixed

--- app/src/main/java/com/tatoeapps/tracktimer/main/MainActivity.kt
+++ app/src/main/java/com/tatoeapps/tracktimer/main/MainActivity.kt
@@ -719,12 +719,17 @@ class MainActivity : AppCompatActivity(),
 
     private fun setUpFullScreen() {
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)act {
-//            window.setDecorFitsSystemWindows(false)
-//        } else {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility =
+                    //this one does immersive mode
+                    (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    //These hide nav and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
 //        }
     }
 
